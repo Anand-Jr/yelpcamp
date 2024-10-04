207c04750f79dfda4a85206bd666be37f6d0de5e
Anand Raj <justmethere333@gmail.com>
2024-10-04 22:51:17 +0530

    updated ejs

--- seeds/index.js
+++ seeds/index.js
@@ -17,11 +17,16 @@ const seedDB = async() => {
     await Campground.deleteMany({});
     for (let i =0; i< 50 ;i++){
         const rand1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) +10;
         const camp =new Campground({
             location :`${cities[rand1000].city}, ${cities[rand1000].state}`,
-            title:`${sample(descriptors)} ${sample(places)}`
-        });
-        await camp.save();     
+            title:`${sample(descriptors)} ${sample(places)}`,
+            image: `https://picsum.photos/400?random=${Math.random()}`,
+            description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+            price: price
+            });
+             
+            await camp.save();     
     }
     //const c = new Campground({title :'campekl gound'});
     
