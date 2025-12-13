47d699ba8f8097c8533aa98a0a664bce4c4db32e
Anand Raj <46198920+Anand-Jr@users.noreply.github.com>
2025-12-13 23:58:49 +0530

    updated error fix

--- controllers/users.js
+++ controllers/users.js
@@ -4,7 +4,7 @@ module.exports.renderRegister = (req, res) => {
     res.render('users/register');
 }
 
-module.exports.register = async (req, res) => {
+module.exports.register = async (req, res, next) => {
     try {
         const { username, email, password } = req.body;
         const user = new User({ email, username });
